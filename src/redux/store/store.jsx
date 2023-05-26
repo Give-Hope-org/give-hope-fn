27f55b26d06cf18199591cc3e46418a91750af0f
Bowlu <awoyemiboluwatife@gmail.com>
2023-05-26 21:25:35 +0100

    chore: setup-tailwind

--- src/redux/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "../reducer/userReducer";
-export default configureStore({
-    reducer: {
-        // reducers
-        user: userSlice
-    }
-});
