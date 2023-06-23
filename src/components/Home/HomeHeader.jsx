474c5a35c4433ebe7330fb7ec713b6166f39a8b4
ALVIN CODER <andelarwanda@Andelas-MacBook-Pro.local>
2023-06-23 12:28:32 +0200

    Added CI/CD

--- src/components/Home/HomeHeader.jsx
+++ src/components/Home/HomeHeader.jsx
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 function HomeHeader() {
   const [showMenu, setShowMenu] = useState(true);
   const [showRegisterPopup, setShowRegisterPopup] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
   const handleRegisterClick = () => {
     setShowRegisterPopup(true);
   };
@@ -14,18 +16,22 @@ function HomeHeader() {
     setShowRegisterPopup(false);
   };
 
+  const handleMobileMenuToggle = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
   useEffect(() => {
     let prevScrollPos = window.pageYOffset;
-    
+
     const handleScroll = () => {
       const currentScrollPos = window.pageYOffset;
       const visible = prevScrollPos > currentScrollPos;
       setShowMenu(visible);
       prevScrollPos = currentScrollPos;
     };
-  
+
     window.addEventListener("scroll", handleScroll);
-  
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -41,21 +47,31 @@ function HomeHeader() {
       </div>
 
       {/* Navigation links */}
-      <ul className="nav__menu">
+      <ul className={`nav__menu ${showMobileMenu ? "show" : ""}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={handleMobileMenuToggle}>
+            Home
+          </Link>
         </li>
         <li>
-          <a href="#charities">Charities</a>
+          <a href="#charities" onClick={handleMobileMenuToggle}>
+            Charities
+          </a>
         </li>
         <li>
-          <a href="#causes">Causes</a>
+          <a href="#causes" onClick={handleMobileMenuToggle}>
+            Causes
+          </a>
         </li>
         <li>
-          <Link to="/about-us">About Us</Link>
+          <Link to="/about-us" onClick={handleMobileMenuToggle}>
+            About Us
+          </Link>
         </li>
         <li>
-          <Link to="/contact-us">Contact Us</Link>
+          <Link to="/contact-us" onClick={handleMobileMenuToggle}>
+            Contact Us
+          </Link>
         </li>
       </ul>
       <div className="flex gap-4">
@@ -71,41 +87,40 @@ function HomeHeader() {
         </Link>
       </div>
       {/* Hamburger menu */}
-      <div className="nav__menu-icon">
+      <div className="nav__menu-icon" onClick={handleMobileMenuToggle}>
         <i className="fas fa-bars"></i>
       </div>
 
       {showRegisterPopup && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-        <div className="register-popup bg-white rounded-md p-6 shadow-lg">
-          <h2 className="text-2xl font-semibold mb-4">Register as:</h2>
-          <div className="flex gap-4">
+          <div className="register-popup bg-white rounded-md p-6 shadow-lg">
+            <h2 className="text-2xl font-semibold mb-4">Register as:</h2>
+            <div className="flex gap-4">
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded"
+                onClick={() => {
+                  window.location.href = "/register-as-org";
+                }}
+              >
+                Organization
+              </button>
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded"
+                onClick={() => {
+                  window.location.href = "/register-as-individual";
+                }}
+              >
+                Individual
+              </button>
+            </div>
             <button
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded"
-              onClick={() => {
-                window.location.href = "/register-as-org";
-              }}
+              className="text-gray-600 hover:text-gray-800 font-semibold mt-4"
+              onClick={closeModal}
             >
-              Organization
-            </button>
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded"
-              onClick={() => {
-                window.location.href = "/register-as-individual";
-              }}
-            >
-              Individual
+              Cancel
             </button>
           </div>
-          <button
-            className="text-gray-600 hover:text-gray-800 font-semibold mt-4"
-            onClick={closeModal}
-          >
-            Cancel
-          </button>
         </div>
-      </div>
-      
       )}
     </nav>
   );
